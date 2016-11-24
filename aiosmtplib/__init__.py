d041cb994ce567d8bb2180069fd60e4f9af03124
Cole Maclean <hi@cole.io>
2016-11-24 15:46:02 -0800

    Fixed __main__ method

--- aiosmtplib/__init__.py
+++ aiosmtplib/__init__.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from .smtp import SMTP
 from .errors import (
     SMTPException, SMTPServerDisconnected, SMTPConnectError,
@@ -15,37 +13,3 @@ __all__ = (
     'SMTPDataError', 'SMTPAuthenticationError', 'SMTPSenderRefused',
     'SMTPRecipientRefused', 'SMTPRecipientsRefused',
 )
-
-
-def main():
-    import sys
-
-    def prompt(prompt):
-        sys.stdout.write(prompt + ": ")
-        sys.stdout.flush()
-        return sys.stdin.readline().strip()
-
-    sender = prompt("From")
-    recipients = prompt("To").split(',')
-    print("Enter message, end with ^D:")
-    message = []
-    while True:
-        line = sys.stdin.readline()
-        if line:
-            message.append(line)
-        else:
-            break
-
-    message = '\n'.join(message)
-    print("Message length is %d" % len(message))
-
-    loop = asyncio.get_event_loop()
-    smtp = SMTP(hostname='localhost', port=25, loop=loop)
-    send_message = asyncio.async(smtp.sendmail(sender, recipients, message))
-    loop.run_until_complete(send_message)
-
-
-# Test the sendmail method, which tests most of the others.
-# Note: This always sends to localhost.
-if __name__ == '__main__':
-    main()
---
+++ aiosmtplib/__main__.py
@@ -0,0 +1,36 @@
+import asyncio
+import sys
+from .smtp import SMTP
+
+
+def prompt(prompt):
+    sys.stdout.write(prompt + ': ')
+    sys.stdout.flush()
+    return sys.stdin.readline().strip()
+
+async def send(hostname, port, sender, recipients, message, loop=None):
+    smtp = SMTP(hostname=hostname, port=port, loop=loop)
+    async with smtp:
+        await smtp.sendmail(sender, recipients, message)
+
+
+hostname = prompt('SMTP server hostname [localhost]') or 'localhost'
+port = prompt('SMTP server port [25]') or 25
+sender = prompt('From')
+recipients = prompt('To').split(',')
+print('Enter message, end with ^D:')
+message = []
+
+while True:
+    line = sys.stdin.readline()
+    if line:
+        message.append(line)
+    else:
+        break
+
+message = '\n'.join(message)
+print('Message length is {}'.format(len(message)))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(
+    send(hostname, port, sender, recipients, message, loop=loop))
