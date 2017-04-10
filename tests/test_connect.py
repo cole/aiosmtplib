3cd613bf10be9c3f83e2e020431e466acbf4053c
Cole Maclean <hi@cole.io>
2017-04-09 19:38:15 -0700

    Fixed disconnect test, try 2

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -133,11 +133,12 @@ async def test_disconnected_server_raises_on_client_write(
     await preset_client.connect()
 
     preset_server.responses.append(b'250 noop')
-    # due to our weird server loop, it's easier just to drop on second read
     preset_server.drop_connection_before_next_read = True
 
-    await preset_client.execute_command(b'NOOP')
+    # There seems to be a race condition here where the thread doesn't always
+    # drop the connection right away, so just try twice, and one will fail
     with pytest.raises(SMTPServerDisconnected):
+        await preset_client.execute_command(b'NOOP')
         await preset_client.execute_command(b'NOOP')
 
     preset_client.close()
