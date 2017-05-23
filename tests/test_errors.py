fe0ebbf7aba30c73d25d0e6de69dc3a62c3fc9ee
Cole Maclean <hi@cole.io>
2017-05-22 18:24:41 -0700

    Hypothesis tests for non-async classes

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -2,6 +2,8 @@
 Test error class imports, arguments, and inheritance.
 """
 import pytest
+from hypothesis import given
+from hypothesis.strategies import integers, lists, text
 
 from aiosmtplib import (
     SMTPAuthenticationError, SMTPConnectError, SMTPDataError, SMTPException,
@@ -15,22 +17,17 @@ CONNECTION_EXCEPTIONS = (SMTPServerDisconnected, SMTPConnectError)
 SIMPLE_RESPONSE_EXCEPTIONS = (
     SMTPNotSupported, SMTPHeloError, SMTPDataError, SMTPAuthenticationError,
 )
-ERROR_CODES = (
-    (503, 'Bad command sequence'),
-    (530, 'Access denied'),
-)
-EMAIL_ADDRESSES = ('a@example.com', 'b@example.com')
 
 
-@pytest.mark.parametrize('code, message', ERROR_CODES)
-def test_raise_smtp_exception(code, message):
+@given(text())
+def test_raise_smtp_exception(message):
     with pytest.raises(SMTPException) as excinfo:
         raise SMTPException(message)
 
     assert excinfo.value.message == message
 
 
-@pytest.mark.parametrize('code, message', ERROR_CODES)
+@given(integers(), text())
 def test_raise_smtp_response_exception(code, message):
     with pytest.raises(SMTPResponseException) as excinfo:
         raise SMTPResponseException(code, message)
@@ -40,9 +37,9 @@ def test_raise_smtp_response_exception(code, message):
     assert excinfo.value.message == message
 
 
-@pytest.mark.parametrize('code, message', ERROR_CODES)
 @pytest.mark.parametrize('error_class', CONNECTION_EXCEPTIONS)
-def test_simple_exceptions(code, message, error_class):
+@given(code=integers(), message=text())
+def test_connection_exceptions(code, message, error_class):
     with pytest.raises(error_class) as excinfo:
         raise error_class(message)
 
@@ -51,8 +48,8 @@ def test_simple_exceptions(code, message, error_class):
     assert excinfo.value.message == message
 
 
-@pytest.mark.parametrize('code, message', ERROR_CODES)
 @pytest.mark.parametrize('error_class', SIMPLE_RESPONSE_EXCEPTIONS)
+@given(code=integers(), message=text())
 def test_simple_response_exceptions(code, message, error_class):
     with pytest.raises(error_class) as excinfo:
         raise error_class(code, message)
@@ -62,8 +59,7 @@ def test_simple_response_exceptions(code, message, error_class):
     assert excinfo.value.message == message
 
 
-@pytest.mark.parametrize('code, message', ERROR_CODES)
-@pytest.mark.parametrize('sender', EMAIL_ADDRESSES)
+@given(integers(), text(), text())
 def test_raise_smtp_sender_refused(code, message, sender):
     with pytest.raises(SMTPSenderRefused) as excinfo:
         raise SMTPSenderRefused(code, message, sender)
@@ -74,8 +70,7 @@ def test_raise_smtp_sender_refused(code, message, sender):
     assert excinfo.value.sender == sender
 
 
-@pytest.mark.parametrize('code, message', ERROR_CODES)
-@pytest.mark.parametrize('recipient', EMAIL_ADDRESSES)
+@given(integers(), text(), text())
 def test_raise_smtp_recipient_refused(code, message, recipient):
     with pytest.raises(SMTPRecipientRefused) as excinfo:
         raise SMTPRecipientRefused(code, message, recipient)
@@ -86,9 +81,10 @@ def test_raise_smtp_recipient_refused(code, message, recipient):
     assert excinfo.value.recipient == recipient
 
 
-def test_raise_smtp_recipients_refused():
+@given(lists(elements=text()))
+def test_raise_smtp_recipients_refused(addresses):
     with pytest.raises(SMTPRecipientsRefused) as excinfo:
-        raise SMTPRecipientsRefused(EMAIL_ADDRESSES)
+        raise SMTPRecipientsRefused(addresses)
 
     assert issubclass(excinfo.type, SMTPException)
-    assert excinfo.value.recipients == EMAIL_ADDRESSES
+    assert excinfo.value.recipients == addresses
--- tests/test_response.py
+++ tests/test_response.py
@@ -1,11 +1,16 @@
+from hypothesis import given
+from hypothesis.strategies import integers, text
+
 from aiosmtplib.response import SMTPResponse
 
 
-def test_response_repr():
-    response = SMTPResponse(250, 'OK')
+@given(integers(), text())
+def test_response_repr(code, message):
+    response = SMTPResponse(code, message)
     assert repr(response) == '({}, {})'.format(response.code, response.message)
 
 
-def test_response_str():
-    response = SMTPResponse(250, 'OK')
+@given(integers(), text())
+def test_response_str(code, message):
+    response = SMTPResponse(code, message)
     assert str(response) == '{} {}'.format(response.code, response.message)
