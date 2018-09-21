6a518d184ba95d732a7c4a19ef01fb5ec95e3947
Cole Maclean <hi@cole.io>
2018-09-20 20:07:08 -0700

    Simplified __main__ (still no tests ☹️))

--- src/aiosmtplib/__main__.py
+++ src/aiosmtplib/__main__.py
@@ -1,39 +1,30 @@
 from aiosmtplib.connection import SMTP_PORT
-from aiosmtplib.errors import SMTPException
 from aiosmtplib.smtp import SMTP
 
 
-def main() -> None:
-    hostname = input('SMTP server hostname [localhost]: ')  # nosec
-    port = input('SMTP server port [{}]: '.format(SMTP_PORT))  # nosec
-    sender = input('From: ')  # nosec
-    recipients = input('To: ').split(',')  # nosec
+raw_hostname = input('SMTP server hostname [localhost]: ')  # nosec
+raw_port = input('SMTP server port [{}]: '.format(SMTP_PORT))  # nosec
+raw_sender = input('From: ')  # nosec
+raw_recipients = input('To: ')  # nosec
 
-    print('Enter message, end with ^D:')
-    lines = []
+hostname = raw_hostname or 'localhost'
+port = int(raw_port) if raw_port else SMTP_PORT
+recipients = raw_recipients.split(',')
+lines = []
 
-    while True:
-        try:
-            lines.append(input())  # nosec
-        except EOFError:
-            break
-
-    message = '\n'.join(lines)
-    message_len = len(message.encode('utf-8'))
-
-    print('Message length (bytes): {}'.format(message_len))
-
-    smtp_client = SMTP(
-        hostname=hostname or 'localhost',
-        port=int(port) if port else SMTP_PORT)
+print('Enter message, end with ^D:')
+while True:
     try:
-        sendmail_errors, sendmail_response = smtp_client.sendmail_sync(
-            sender, recipients, message)
-    except SMTPException as exc:
-        print('{}: {}'.format(exc.__class__.__name__, exc))
-    else:
-        print('Server response: {}'.format(sendmail_response))
+        lines.append(input())  # nosec
+    except EOFError:
+        break
 
+message = '\n'.join(lines)
 
-if __name__ == '__main__':
-    main()
+print('Message length (bytes): {}'.format(len(message.encode('utf-8'))))
+
+smtp_client = SMTP(hostname=hostname or 'localhost', port=port)
+sendmail_errors, sendmail_response = smtp_client.sendmail_sync(
+    raw_sender, recipients, message)
+
+print('Server response: {}'.format(sendmail_response))
