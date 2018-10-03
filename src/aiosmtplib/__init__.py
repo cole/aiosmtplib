5a5b0836b4c7b17a9e4af2bf0aeef9b1bcf35dd4
Cole Maclean <hi@cole.io>
2018-10-02 19:59:56 -0700

    Bump version for next release (1.0.4dev0)

--- src/aiosmtplib/__init__.py
+++ src/aiosmtplib/__init__.py
@@ -16,7 +16,7 @@ from .status import *  # NOQA
 
 
 __title__ = "aiosmtplib"
-__version__ = "1.0.3"
+__version__ = "1.0.4dev0"
 __author__ = "Cole Maclean"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018 Cole Maclean"
