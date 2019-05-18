644c4c6d09c147a073e23e1bf1a61c6a643d3a04
Cole Maclean <hi@cole.io>
2019-05-18 13:51:17 -0700

    Remove asserts

--- src/aiosmtplib/protocol.py
+++ src/aiosmtplib/protocol.py
@@ -40,11 +40,9 @@ class FlowControlMixin(object):
         self._connection_lost = False
 
     def pause_writing(self):
-        assert not self._paused
         self._paused = True
 
     def resume_writing(self):
-        assert self._paused
         self._paused = False
 
         waiter = self._drain_waiter
@@ -74,8 +72,6 @@ class FlowControlMixin(object):
             raise ConnectionResetError("Connection lost")
         if not self._paused:
             return
-        waiter = self._drain_waiter
-        assert waiter is None or waiter.cancelled()
         waiter = self._loop.create_future()
         self._drain_waiter = waiter
         await waiter
