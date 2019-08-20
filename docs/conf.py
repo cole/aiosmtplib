4f83d6cff36d08b70e220f742173545fa41028ad
Cole Maclean <hi@cole.io>
2019-08-20 06:08:42 -0700

    Unset unused static path

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import os
 import pathlib
 import re
 import sys
-from typing import Dict
+from typing import Dict, List
 
 
 sys.path.insert(0, os.path.abspath("../src"))
@@ -125,7 +125,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []  # type: List[str]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
