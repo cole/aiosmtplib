4bcb9c0baf007bee6e887f609dc4376e35d1acd6
Cole Maclean <hi@colemaclean.dev>
2022-10-15 16:44:05 -0700

    chore: 2.0rc0

--- aiosmtplib/__init__.py
+++ aiosmtplib/__init__.py
@@ -32,7 +32,7 @@ from .typing import SMTPStatus
 
 
 __title__ = "aiosmtplib"
-__version__ = "2.0.0dev0"
+__version__ = "2.0.0rc0"
 __author__ = "Cole Maclean"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022 Cole Maclean"
